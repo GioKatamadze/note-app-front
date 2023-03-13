10ff20facd68e4ab77050d3d39f9e1f59fb17bdf
GioKatamadze <giokat.inbox@gmail.com>
2023-03-13 04:34:31 +0400

    fix external reach problem for web page and add preview image

--- src/routing/protectedRoute.js
+++ src/routing/protectedRoute.js
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import SignIn from "../Pages/sign-in/signin.jsx";
+import { useNavigate } from "react-router-dom";
 
 const ProtectedRoute = () => {
+  const navigate = useNavigate();
   if (!localStorage.token) {
-    return <SignIn />;
+    navigate("/signin");
   }
   return <Outlet />;
 };
